c20f8603c7d1bf6edf5bfa1b51e1e818d0bf7b67
sunnirvana <yubogo@gmail.com>
2016-11-05 22:01:51 +0800

    first commit

---
+++ structural/decorator/RendererInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DesignPatterns\Structural\Decorator;
+
+/**
+ * RendererInterface接口
+ */
+interface RendererInterface
+{
+    /**
+     * render data
+     *
+     * @return mixed
+     */
+    public function renderData();
+}
